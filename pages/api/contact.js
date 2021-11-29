617cc57c51f585e0454dfe7f2d60b73fb5ad34ae
nawesan12 <nahueltecnicomdp@gmail.com>
2021-11-29 13:04:08 -0300

    wawa

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,33 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function (req, res) {
-    console.log(req.body)
+    require('dotenv').config()
+    const nodemailer = require('nodemailer');
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: "smtp.gmail.com",
+        auth: {
+          user: 'demo@demo.gmail',
+          pass: 'password',
+        },
+        secure: true,});
+
+        const mailData = {
+            from: 'demo@demo.com',
+            to: 'your email',
+            subject: `Message From ${req.body.name}`,
+            text: req.body.message,
+            html: <div>{req.body.message}</div>
+        }
+
+        transporter.sendMail(mailData, (err, info) => {
+            if (err) {
+                console.log(err)
+                res.send(err)
+            } else {
+                console.log(info)
+                res.send(info)
+            }
+        })
+
+        res.status(200)
 }
